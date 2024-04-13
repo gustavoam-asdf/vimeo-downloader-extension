bdf27174ce8f8b95c8996def2de7463857ec2773
Gustavo A <gustavo.atencio@outlook.com>
2024-04-13 12:01:34 -0500

    include sw to compile

---
+++ src/sw.ts
@@ -0,0 +1,39 @@
+//import { FFmpeg } from '@ffmpeg/ffmpeg'
+
+chrome.webRequest.onCompleted.addListener(
+	async details => {
+		const isMasterJsonRequest = details.url.includes('master.json')
+		if (!isMasterJsonRequest) return
+
+		await chrome.storage.session.set({
+			["new-master-json"]: {
+				tabId: details.tabId,
+				url: details.url
+			},
+		})
+	},
+	{
+		urls: [
+			'<all_urls>'
+		],
+	},
+)
+
+//const ffmpeg = new FFmpeg()
+
+chrome.storage.session.onChanged.addListener(changes => {
+	const downloadedVideo = changes["downloaded-video"]
+	if (!downloadedVideo) return
+
+	/**
+	 * @type {{
+	* 	oldValue: unknown | undefined
+	* 	newValue: import("crypto").UUID | undefined
+	* }}
+	*/
+	const { newValue } = downloadedVideo
+
+	if (!newValue) return
+
+	console.log({ newValue })
+});
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,19 @@ import react from "@vitejs/plugin-react-swc"
 
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		rollupOptions: {
+			input: {
+				main: path.resolve(__dirname, "index.html"),
+				sw: path.resolve(__dirname, "src/sw.ts"),
+			},
+			output: {
+				entryFileNames: "assets/[name].js",
+				chunkFileNames: "assets/[name].js",
+				assetFileNames: "assets/[name].[ext]",
+			}
+		}
+	},
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
