0b8739f042563ef40171d4b6cb18afda6d2361c5
Gustavo A <gustavo.atencio@outlook.com>
2024-07-21 00:20:59 -0500

    order responsability

--- src/hooks/useVimeoDownloader.ts
+++ src/hooks/useVimeoDownloader.ts
@@ -1,11 +1,5 @@
-import { useCallback, useEffect, useState } from "react"
-
-import { MasterVideo } from "@/models/MasterVideo"
 import { MediaResolved } from "@/models/MediaResolved"
-import { SegmentResolved } from "@/models/SegmentResolved"
-import { UUID } from "crypto"
-import { fetchWithRetry } from "@/lib/fetchWithRetry"
-import { splitInChunks } from "@/lib/splitInChunks"
+import { useState } from "react"
 
 interface DownloadState {
 	video: {
@@ -18,27 +12,7 @@ interface DownloadState {
 	}
 }
 
-interface VideoResourcesResolved {
-	videoId: UUID
-	video: MediaResolved
-	audio?: MediaResolved
-}
-
-const VIDEO_MAX_HEIGHT = 1080
-
-export function useVimeoDownloader(resolvedMasterJsonUrl?: string) {
-	const [betterVideoResourcesResolved, setBetterVideoResourcesResolved] = useState<VideoResourcesResolved>()
-
-	const [masterJsonUrl, setMasterJsonUrl] = useState(resolvedMasterJsonUrl)
-
-	useEffect(() => {
-		if (!resolvedMasterJsonUrl) {
-			setBetterVideoResourcesResolved(undefined)
-			return
-		}
-		setMasterJsonUrl(resolvedMasterJsonUrl)
-	}, [resolvedMasterJsonUrl])
-
+export function useVimeoDownloader() {
 	const [downloadState, setDownloadState] = useState<DownloadState>({
 		video: {
 			isDownloading: false,
@@ -50,92 +24,13 @@ export function useVimeoDownloader(resolvedMasterJsonUrl?: string) {
 		}
 	})
 
-	useEffect(() => {
-		if (!masterJsonUrl) return
-
-		const getBetterMedia = async () => {
-			const masterUrl = new URL(masterJsonUrl).toString()
-
-			const cachedMap = await chrome.storage.session.get(masterUrl)
-
-			const cached = cachedMap[masterUrl] as VideoResourcesResolved | undefined
-
-			if (cached) {
-				return cached
-			}
-
-			const response = await fetchWithRetry({ url: masterUrl })
-			const master = await response.json() as MasterVideo
-
-			const availableVideos = [...master.video]
-				.filter(video => video.height <= VIDEO_MAX_HEIGHT)
-				.sort((a, b) => a.avg_bitrate - b.avg_bitrate)
-			const availableAudios = master.audio
-				? [...master.audio].sort((a, b) => a.avg_bitrate - b.avg_bitrate)
-				: undefined
-
-			const betterVideo = availableVideos[availableVideos.length - 1]
-			const betterAudio = availableAudios ? availableAudios[availableAudios.length - 1] : undefined
-
-			const mediaUrl = new URL(master.base_url, masterUrl).href
-
-			const videoResolved: MediaResolved = {
-				...betterVideo,
-				absoluteUrl: mediaUrl,
-				segments: betterVideo.segments.map(segment => ({
-					...segment,
-					absoluteUrl: `${mediaUrl}${betterVideo.base_url}${segment.url}`
-				}))
-			}
-
-			const audioResolved: MediaResolved | undefined = betterAudio
-				? {
-					...betterAudio,
-					absoluteUrl: mediaUrl,
-					segments: betterAudio.segments.map(segment => ({
-						...segment,
-						absoluteUrl: `${mediaUrl}${betterAudio.base_url}${segment.url}`
-					}))
-				}
-				: undefined
-
-			const mediaResourcesResolved: VideoResourcesResolved = {
-				videoId: master.clip_id,
-				video: videoResolved,
-				audio: audioResolved
-			}
-
-			await chrome.storage.session.set({
-				[masterUrl]: mediaResourcesResolved
-			})
-
-			return mediaResourcesResolved
-		}
-
-		getBetterMedia()
-			.then(setBetterVideoResourcesResolved)
-	}, [masterJsonUrl])
-
-	const downloadSegment = useCallback(
-		async (segment: SegmentResolved) => {
-			const response = await fetchWithRetry({
-				url: segment.absoluteUrl
-			});
-			if (!response.ok) {
-				throw new Error(`Downloading segment with url '${segment.absoluteUrl}' failed with status: ${response.status} ${response.statusText}`);
-			}
-			return await response.blob();
-		},
-		[]
-	)
-
-	const processMedia = async ({
+	const downloadMedia = async ({
 		type,
 		media
 	}: {
 		type: 'video' | 'audio',
 		media: MediaResolved
-	}): Promise<Blob> => {
+	}): Promise<ReadableStream<Uint8Array>> => {
 		setDownloadState(prev => ({
 			...prev,
 			[type]: {
@@ -144,88 +39,59 @@ export function useVimeoDownloader(resolvedMasterJsonUrl?: string) {
 			}
 		}))
 
-		const totalSegments = media.segments.length + 1
-		const segmentSize = 100 / totalSegments
+		let segmentIndex = 0
+		const stream = new ReadableStream({
+			start(controller) {
+				const initSegment = media.init_segment
+				const initContent = Uint8Array.from(globalThis.atob(initSegment), c => c.charCodeAt(0))
 
-		const chunkSize = Math.ceil(totalSegments / 10) + 1
+				controller.enqueue(initContent)
+			},
+			async pull(controller) {
+				const segment = media.segments[segmentIndex]
 
-		const rawInitContent = window.atob(media.init_segment)
-		const initContentData = Uint8Array.from(rawInitContent, c => c.charCodeAt(0))
+				if (!segment) {
+					controller.close()
 
-		const initContent = new Blob([initContentData], { type: media.mime_type });
+					setDownloadState(prev => ({
+						...prev,
+						[type]: {
+							isDownloading: false,
+							progress: 100
+						}
+					}))
 
-		setDownloadState(prev => ({
-			...prev,
-			[type]: {
-				isDownloading: true,
-				progress: segmentSize
-			}
-		}))
-
-		const chunks = splitInChunks({
-			values: media.segments,
-			size: chunkSize
-		})
-
-		let mediaContent = new Blob([initContent], { type: media.mime_type });
-
-		for await (const { values: segments } of chunks) {
-			const downloadedSegments = await Promise.all(
-				segments.map(downloadSegment)
-			)
-			const result = new Blob(downloadedSegments, { type: media.mime_type });
-
-			mediaContent = new Blob([mediaContent, result], { type: media.mime_type });
-
-			setDownloadState(prev => ({
-				...prev,
-				[type]: {
-					isDownloading: true,
-					progress: prev[type].progress + segmentSize * segments.length
+					return
 				}
-			}))
-		}
 
-		setDownloadState(prev => ({
-			...prev,
-			[type]: {
-				isDownloading: false,
-				progress: 100
+				const response = await fetch(segment.absoluteUrl)
+
+				if (!response.ok) {
+					controller.error(new Error(`[${response.status}] Failed to fetch ${segment.absoluteUrl}: ${response.statusText}`))
+				}
+
+				const content = new Uint8Array(await response.arrayBuffer())
+
+				controller.enqueue(content)
+
+				setDownloadState(prev => ({
+					...prev,
+					[type]: {
+						isDownloading: true,
+						progress: prev[type].progress + segmentIndex * 100 / media.segments.length
+					}
+				}))
+
+				segmentIndex++
 			}
-		}))
-
-		return mediaContent
-	}
-
-	const processVideoMedia = async (video: MediaResolved) => {
-		if (downloadState.video.isDownloading) {
-			console.warn('Video is already downloading, skipping')
-			return
-		}
-
-		return await processMedia({
-			type: 'video',
-			media: video
 		})
-	}
 
-	const processAudioMedia = async (audio: MediaResolved) => {
-		if (downloadState.audio.isDownloading) {
-			console.warn('Audio is already downloading, skipping')
-			return
-		}
-
-		return await processMedia({
-			type: 'audio',
-			media: audio
-		})
+		return stream
 	}
 
 	return {
 		downloadState,
-		videoResourcesResolved: betterVideoResourcesResolved,
-		processVideoMedia,
-		processAudioMedia,
+		downloadMedia,
 	}
 
 }
