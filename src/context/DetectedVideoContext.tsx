c7ecfb32f9676964d3114bb096538982554de031
Gustavo A <gustavo.atencio@outlook.com>
2024-07-21 00:50:39 -0500

    get latest detected video info

--- src/context/DetectedVideoContext.tsx
+++ src/context/DetectedVideoContext.tsx
@@ -52,13 +52,15 @@ export function DetectVideoContextProvider({
 	}, [currentTab])
 
 	useEffect(() => {
-		const listener = (message: unknown) => {
+		const listener = async (message: unknown) => {
 			if (!currentTab) return
 
 			if (typeof message !== 'object' || message === null) return
 			const isMasterJsonMessage = (message as Record<string, string>).type === 'new-master-json'
 
 			if (!isMasterJsonMessage) return
+
+			
 
 			const { tabId, url } = message as {
 				tabId: number
@@ -67,19 +69,43 @@ export function DetectVideoContextProvider({
 
 			if (tabId !== currentTab?.id) return
 
-			setDetectedVideoInfo({
+			const newDetectedVideoInfo: DetectedVideoInfo = {
 				tab: {
 					id: tabId,
 					url: currentTab.url!,
 				},
 				name: currentTab.title ?? 'video-detectado',
 				masterJsonUrl: url,
+			}
+
+			setDetectedVideoInfo(newDetectedVideoInfo)
+
+			await chrome.storage.session.set({
+				[newDetectedVideoInfo.tab.url]: newDetectedVideoInfo
 			})
 		}
 
 		chrome.runtime.onMessage.addListener(listener)
 
 		return () => chrome.runtime.onMessage.removeListener(listener)
+	}, [currentTab])
+
+	useEffect(() => {
+		const getLastDetectedVideoInfo = async () => {
+			if (!currentTab) return
+
+			const cachedMap = await chrome.storage.session.get(currentTab.url)
+
+			const cached = cachedMap[currentTab.url!] as DetectedVideoInfo | undefined
+
+			if (!cached) return
+
+			if (cached.tab.id !== currentTab.id) return
+
+			setDetectedVideoInfo(cached)
+		}
+
+		getLastDetectedVideoInfo()
 	}, [currentTab])
 
 	return (
